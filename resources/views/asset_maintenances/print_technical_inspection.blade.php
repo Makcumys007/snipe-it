686ffca13a7f7d2b1627a7736865ea7bead2e3e8
Maxim Abylkassov <maxim.abylkassov@gmail.com>
2024-08-28 10:44:13 +0000

    print_technical_inspection.blade

--- resources/views/asset_maintenances/print_technical_inspection.blade.php
+++ resources/views/asset_maintenances/print_technical_inspection.blade.php
@@ -3,41 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Документ</title>
+    <title>Акт технического осмотра</title>
     <style>
         body {
             font-family: Arial, sans-serif;
             margin: 20px;
         }
-        .header {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-        .header img {
-            width: 150px;
-        }
-        .title {
-            text-align: center;
-            font-size: 24px;
-            margin-top: 20px;
-        }
-        .content {
-            margin-top: 20px;
-        }
-        .content p {
-            margin: 10px 0;
-        }
-        .right-align {
+        .header, .footer {
             text-align: right;
         }
-        .table {
-            width: 100%;
-            border-collapse: collapse;
+        .header img {
+            float: left;
+            width: 150px;
+        }
+        .section {
             margin-top: 20px;
         }
-        .table th, .table td {
-            border: 1px solid #000;
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 10px;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
             padding: 8px;
             text-align: left;
         }
@@ -46,13 +36,14 @@
             display: flex;
             justify-content: space-between;
         }
-        .signatures div {
-            margin-bottom: 20px;
-        }
-
         .bold-underline {
             font-weight: bold;
             text-decoration: underline;
+        }
+        .title {
+            text-align: center;
+            font-size: 24px;
+            margin-top: 20px;
         }
     </style>
 </head>
@@ -60,19 +51,22 @@
     <div class="header"><img class="navbar-brand-img"
                                              src="{{ Storage::disk('public')->url($snipeSettings->logo) }}"
                                              alt="{{ $snipeSettings->site_name }} logo">
+        <p>Приложение №4</p>
     </div>
+
+    <div class="section">
+        <p class="header">ТОО «Казминерале Бозшаколь»</p>
+        <p class="header">Начальник отдела информационных технологий<br>ФИО/подпись ________________/___________</p>
+        <p class="header">{{ \App\Helpers\Helper::getFormattedDateObject($assetMaintenance->start_date, 'd.m.Y', false) }}</p>
+    </div>
+
+    <div class="section">
     <div class="title">
         Акт технического осмотра
     </div>
-    <div class="content">
-        <p class="right-align">ТОО «Казминералс Бозшаколь»</p>
-        <p class="right-align">ФИО/подпись ________________/___________</p>
-        <p class="right-align">Начальник отдела информационных технологий</p>
-        <p class="right-align">{{ \App\Helpers\Helper::getFormattedDateObject($assetMaintenance->start_date, 'd.m.Y', false) }}</p>
-        <br>
-        <br>
-        <p>Я, ниже подписавшийся <span class="bold-underline">{{ optional($support)->first_name ?? '' }} {{ optional($support)->last_name ?? '' }}, {{ optional($support)->jobtitle ?? '' }}</span>, составил настоящий акт о том, что для диагностики представлено следующее оборудование:</p>
-        <table class="table">
+        <p>Я, ниже подписавшийся <span class="bold-underline">{{ optional($support)->first_name ?? '' }} {{ optional($support)->last_name ?? '' }}, {{ optional($support)->jobtitle ?? '' }}</span></p>
+        <p>Составил настоящий акт о том, что для диагностики представлено следующее оборудование:</p>
+        <table>
             <tr>
                 <th>Наименование</th>
                 <th>Инвентарный номер</th>
@@ -88,15 +82,14 @@
                 <td>{{ optional($assetMaintenance)->title ?? '' }}</td>
             </tr>
         </table>
-        <br>
-        <br>
         <p class="bold-underline">После произведенной диагностики установлено, что: </p>
         <p >{{ optional($assetMaintenance)->notes ?? ''}}</p>
         <p class="bold-underline">Заключение: </p>
         <p >{{ optional($assetMaintenance)->solution ?? ''}}</p>
     </div>
-    <div class="signatures">
-        <span class="bold-underline">{{ optional($support)->jobtitle ?? '' }}:</span> <span class="right-align">___________________ {{ optional($support)->first_name ?? '' }} {{ optional($support)->last_name ?? '' }}</span>
+
+    <div class="signature">
+        <span class="bold-underline">{{ optional($support)->jobtitle ?? '' }}:</span> <span class="right-align">           ___________________            {{ optional($support)->first_name ?? '' }} {{ optional($support)->last_name ?? '' }}</span>
     </div>
 </body>
 </html>
